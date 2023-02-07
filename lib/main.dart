2453b173a56b07efa705d1db79c79b821a5dc5a6
Lucas Reis <lucasreis_9@hotmail.com>
2023-02-06 23:12:13 -0300

    Ajustes finais

--- lib/main.dart
+++ lib/main.dart
@@ -25,95 +25,101 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    int count = 0;
+  int count = 0;
 
-    void decrement() {
-      setState(() {
-        count--;
-      });
-    }
+  void decrement() {
+    setState(() {
+      count--;
+    });
+  }
 
-    void increment() {
-      setState(() {
+  void increment() {
+    setState(() {
       count++;
-      });
-    }
+    });
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.grey,
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/image/sorvete.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Pode Entrar",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(30),
-                child: Text(
-                  count.toString(),
-                  style: const TextStyle(
-                    fontSize: 100,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      fixedSize: const Size(100, 100),
-                      primary: Colors.black,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16)),
-                    ),
-                    onPressed: decrement,
-                    child: const Text(
-                      "Saiu",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 30),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      fixedSize: const Size(100, 100),
-                      primary: Colors.black,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16)),
-                    ),
-                    onPressed: increment,
-                    child: const Text(
-                      "Entrou",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image/sorvete.jpg'),
+            fit: BoxFit.cover,
           ),
         ),
-      );
-    }
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? "Lotado" : "Pode Entrar",
+              style: TextStyle(
+                fontSize: 30,
+                color: isFull ? Colors.red : Colors.white,
+                backgroundColor: Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Text(
+                count.toString(),
+                style: const TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                  ),
+                  onPressed: isEmpty ? null : decrement,
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 30),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                  ),
+                  onPressed: isFull ? null : increment,
+                  child: const Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
